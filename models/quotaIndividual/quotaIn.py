b584ab483ff7307e0bb849c6231a5d7bdb8e1713
jackone666 <liuwenhuaaimenqibin@gmail.com>
2020-09-27 13:50:55 +0800

    9.22

--- models/quotaIndividual/quotaIn.py
+++ models/quotaIndividual/quotaIn.py
@@ -8,19 +8,19 @@ def quota(data):
     tree, level, map = ParseToMatrix.returnMatrix(data)
     individualQuota = []
     nodeNumber = list(map.index).__len__()  # 1节点个数
-    # individualQuota.append(nodeNumber)
+    individualQuota.append(nodeNumber)
     leafNodeNum = LeafNodeNum.leadNodeNum(tree)  # 2叶子节点个数
-    # individualQuota.append(leafNodeNum)
+    individualQuota.append(leafNodeNum)
     branchesNum = BranchesNum.branchesNum(level)  # 3第二层节点数  貌似论文称为分支个数
-    # individualQuota.append(branchesNum)
+    individualQuota.append(branchesNum)
     maxWith = MaxWith.maxWith(level)  # 4 最大层宽度
-    # individualQuota.append(maxWith)
+    individualQuota.append(maxWith)
     verticalLinksNum = nodeNumber - 1  # 5 纵向连接个数即等级关系的个数
-    # individualQuota.append(verticalLinksNum)
+    individualQuota.append(verticalLinksNum)
     transverseLinksNum = list(map.index).__len__() - list(tree.index).__len__()  # 6横向连接个数即交叉关系的个数
-    # individualQuota.append(transverseLinksNum)
+    individualQuota.append(transverseLinksNum)
     sumWith = SumWith.sumWith(level)  # 7总宽度  树结构每一层的宽度和
-    # individualQuota.append(sumWith)
+    individualQuota.append(sumWith)
     averageWith = sumWith / list(tree.index).__len__()  # 8平均宽度，总宽度除以树结构的总结点
     individualQuota.append(averageWith)
     # entropyTotal = EntropyTotal.entropyTotal(tree)  # 9信息熵总和
@@ -28,30 +28,30 @@ def quota(data):
     # notLeafAverEntropy = entropyTotal / (list(tree.index).__len__()) - leafNodeNum  # 10 非叶子节点平均信息熵
     # individualQuota.append(notLeafAverEntropy)
     nodeLevelNum = NodeLevelNum.nodeLevelNum(level)  # 11层数
-    # individualQuota.append(nodeLevelNum)
+    individualQuota.append(nodeLevelNum)
     maxDepthLeaf = nodeLevelNum  # 12最大叶节点深度
-    # individualQuota.append(maxDepthLeaf)
+    individualQuota.append(maxDepthLeaf)
     totalDepthLeaf = TotalDepthLeaf.totalDepthLeaf(tree, level)  # 13叶节点深度总和
-    # individualQuota.append(totalDepthLeaf)
+    individualQuota.append(totalDepthLeaf)
     rootToLeafNum = leafNodeNum  # 14 根节点到叶节点的路径总条数  等于叶节点个数
-    # individualQuota.append(rootToLeafNum)
+    individualQuota.append(rootToLeafNum)
     averLeafDepth = totalDepthLeaf / rootToLeafNum  # 15平均叶子深度
     individualQuota.append(averLeafDepth)
     pathRootToNodeTotal = PathRootToNodeTotal.pathRootToNodeTotal(level)  # 16路径总深度  即每个节点深度和
-    # individualQuota.append(pathRootToNodeTotal)
+    individualQuota.append(pathRootToNodeTotal)
     pathTotalNum = PathTotalNum.pathTotalNum(tree)  # 17路径总条数     根到所有节点路径个数和
-    # individualQuota.append(pathTotalNum)
+    individualQuota.append(pathTotalNum)
     averPathDepth = pathRootToNodeTotal / pathTotalNum  # 18平均路径深度(16/17)
     individualQuota.append(averPathDepth)
     hitsHResult, hitsAResult = Hits.hits(map)  # 19Hub均值--H 20Authority均值
-    # individualQuota.append(hitsHResult)
-    # individualQuota.append(hitsAResult)
+    individualQuota.append(hitsHResult)
+    individualQuota.append(hitsAResult)
     pageRank = PageRank.pageRank(map)  # 21 pr均值
     individualQuota.append(pageRank)
     knowledgeStorageCapacity = hitsHResult / hitsAResult  # 22 知识存储容量S=H/A
-    # individualQuota.append(knowledgeStorageCapacity)
+    individualQuota.append(knowledgeStorageCapacity)
     netDiameter = NetDiameter.netDiameter(map)  # 23网络直径T
-    # individualQuota.append(netDiameter)
+    individualQuota.append(netDiameter)
     knowledgeDistribution = math.log(netDiameter * hitsHResult * (1 - hitsAResult))  # 24知识分布性D=log(T*H(1-A)
     individualQuota.append(knowledgeDistribution)
     knowledgeSearch = math.sqrt(knowledgeStorageCapacity * pageRank)  # 25知识检索R=√(S*P)
--- z-Score/toExcel/toCluExcel.py
+++ z-Score/toExcel/toCluExcel.py
@@ -1,6 +1,7 @@
 import matplotlib
 from sklearn.cluster import AffinityPropagation
 import pandas as pd
+
 matplotlib.use('TkAgg')  # 大小写无所谓 tkaGg ,TkAgg 都行
 from models.quotaIndividual.quotaIn import quota
 import numpy as np
@@ -8,6 +9,7 @@ import plotly.graph_objects as go
 import xlrd
 import xlwt
 from notebooks import readToChapterJson
+
 book = xlwt.Workbook(encoding="utf-8")  # 创建工作簿
 sheet = book.add_sheet("sheet")  # 创建工作表格
 dataXlsx = xlrd.open_workbook('../data/z-scoreResult/z-Score.xlsx')
@@ -25,7 +27,7 @@ for i in range(3, 15):
     chapterJson, name = readToChapterJson.readToChapterJson(chapter)
     for j in chapterJson.keys():
         if str(chapterJson.get(j)).__contains__("root"):
-            nameTotal.append(name[int(j)-1])
+            nameTotal.append(name[int(j) - 1])
             quotaDic[chapter + j] = quota(chapterJson.get(j))
             print(chapter + "________" + j)
             highScore.append(table.cell(int(j), (i - 2) * 2).value)
@@ -41,7 +43,12 @@ labels = af.labels_
 n_clusters_ = len(cluster_centers_indices)
 print(n_clusters_)
 print(labels)
-cols = ['color', '平均宽度', '平均叶子深度', '平均路径深度', 'pr均值', 'D', 'R']
+cols = ['color', '1节点个数', '2叶子节点个数', '3第二层节点数貌似论文称为分支个数', '4 最大层宽度',
+        '5 纵向连接个数即等级关系的个数', '6横向连接个数即交叉关系的个数', '7总宽度  树结构每一层的宽度和',
+        '8平均宽度，总宽度除以树结构的总结点', ' 9层数', '10最大叶节点深度', '11叶节点深度总和',
+        '12根节点到叶节点的路径总条数', '13平均叶子深度', '14路径总深度', '15路径总条数',
+        '16平均路径深度(16/17)', '17Hub均值--H', '18Authority均值-A',
+        '19 pr均值', '20 知识存储容量S=H/A', '21网络直径T', '22知识分布性D=log(T*H(1-A)', '23知识检索R=√(S*P)']
 index = list(quotaDic.keys())
 npStructData = np.column_stack((labels, list(quotaDic.values())))
 pdStructData = pd.DataFrame(npStructData, columns=cols, index=index)  # pd结构矩阵构造完成
@@ -66,9 +73,14 @@ fig.update_layout(
     plot_bgcolor='white',
     paper_bgcolor='white'
 )
-fig.show()
+# fig.show()
 # 给np矩阵扩充列
-CluIndex = ['姓名', 'highScore', 'lowScore', '类别', '平均宽度', '平均叶子深度', '平均路径深度', 'pr均值', 'D', 'R']
+CluIndex = ['姓名', 'highScore', 'lowScore', '类别', '1节点个数', '2叶子节点个数', '3第二层节点数貌似论文称为分支个数', '4 最大层宽度',
+            '5 纵向连接个数即等级关系的个数', '6横向连接个数即交叉关系的个数', '7总宽度  树结构每一层的宽度和',
+            '8平均宽度，总宽度除以树结构的总结点', ' 9层数', '10最大叶节点深度', '11叶节点深度总和',
+            '12根节点到叶节点的路径总条数', '13平均叶子深度', '14路径总深度', '15路径总条数',
+            '16平均路径深度(16/17)', '17Hub均值--H', '18Authority均值-A',
+            '19 pr均值', '20 知识存储容量S=H/A', '21网络直径T', '22知识分布性D=log(T*H(1-A)', '23知识检索R=√(S*P)']
 merge1 = np.column_stack((lowScore, npStructData))
 merge2 = np.column_stack((highScore, merge1))
 merge3 = np.column_stack((nameTotal, merge2))
